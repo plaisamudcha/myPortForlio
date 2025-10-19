8c6c9f68dabcb5c9effad8e04c202ff428d9aa6c
plaisamudcha <plai_samudcha@hotmail.com>
2025-10-19 16:46:09 +0700

    fixed/svg-error

--- src/components/DotsButton.tsx
+++ src/components/DotsButton.tsx
@@ -4,44 +4,44 @@ import { Dot } from "lucide-react";
 function DotsButton({ ...props }: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div
-      className={cn("w-26 h-26flex flex-col justify-between", {
+      className={cn("w-26 h-26 flex flex-col justify-between", {
         ...props,
       })}
     >
       <div className="flex justify-between h-1/5 items-center">
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
       </div>
       <div className="flex justify-between h-1/5 items-center">
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
       </div>
       <div className="flex justify-between h-1/5 items-center">
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
       </div>
       <div className="flex justify-between h-1/5 items-center">
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
       </div>
       <div className="flex justify-between h-1/5 items-center">
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
-        <Dot color="#ABB2BF" width="auto" height="auto" />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
+        <Dot color="#ABB2BF" size={16} />
       </div>
     </div>
   );
